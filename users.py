28166abca44d13596742d7b79569715a632bb3a2
hillarydalie <hidalie@gmail.com>
2019-09-01 23:08:16 +0300

    Add users class and model to project after testing

--- users.py
+++ users.py
@@ -1,10 +1,20 @@
-class Password:
+class Users:
     """
-    Class that generates new instances of passwords to be saved.
+    Class that generates new instances of users.
     """
 
-    passwords_list = [] #Empty pasword list.
-        #Init the method up here.
+    list_of_users = []
 
-    
-            
+    def __init__(self, user_name,gender, password):
+        self.user_name = user_name
+        self.gender = gender
+        self.password = password
+
+    def save_user(self):
+        '''
+        function that saves User objects into list_of_users
+        '''
+        self.list_of_users.append(self)
+
+
+
