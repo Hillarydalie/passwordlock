5fcd3102408d8a9c05756118d05f502cb11600cd
hillarydalie <hidalie@gmail.com>
2019-09-02 01:41:54 +0300

    Add users tested file to

--- users_test.py
+++ users_test.py
@@ -1,5 +1,5 @@
 import unittest #unittest module imported
-from users import Users #Importing class User from module user
+from user import User #Importing class User from module user
 
 class TestUser(unittest.TestCase):
     '''
@@ -9,15 +9,15 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = Users("firstUser", "Female", "f1924")
+        self.new_user = User("firstUser", "Male", "12@34")
 
     def test_init(self):
           '''
           test_init test case to test if the object is initialized properly
           '''
           self.assertEqual(self.new_user.user_name, "firstUser")
-          self.assertEqual(self.new_user.gender, "Female")
-          self.assertEqual(self.new_user.password, "f1924")
+          self.assertEqual(self.new_user.gender, "Male")
+          self.assertEqual(self.new_user.password, "12@34")
 
 if __name__ == '__main__':
     unittest.main()
