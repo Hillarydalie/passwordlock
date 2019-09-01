069f713245b59b039b4a33058e1c0eb5abb1d8fb
hillarydalie <hidalie@gmail.com>
2019-09-01 14:52:11 +0300

    Rename passwordllock to users

---
+++ users.py
@@ -0,0 +1,10 @@
+class Password:
+    """
+    Class that generates new instances of passwords to be saved.
+    """
+
+    passwords_list = [] #Empty pasword list.
+        #Init the method up here.
+
+    
+            
